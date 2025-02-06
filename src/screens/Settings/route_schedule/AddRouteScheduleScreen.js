2363dd5e28bd2956b05d0c25699eaaf7430a9bc8
Admin <abhay.kumar@bharuwasolutions.com>
2025-02-06 10:54:31 +0530

    dark mode issue resolve

--- src/screens/Settings/route_schedule/AddRouteScheduleScreen.js
+++ src/screens/Settings/route_schedule/AddRouteScheduleScreen.js
@@ -28,7 +28,7 @@ const AddRouteScheduleScreen = () => {
             editable={false}
             style={styles.input}
             label="Date"
-            right={<TextInput.Icon icon="calendar-outline" />}
+            right={<TextInput.Icon icon="calendar-outline" onPress={() => setOpen(true)}/>}
           />
         </Pressable>
         <DatePicker
